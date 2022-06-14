8849c84400eb8d4dd37c939da806670d936a234e
vasil-yordanov14 <vasil_sh@abv.bg>
2022-06-14 23:54:46 +0300

    submenu links

--- app.js
+++ app.js
@@ -33,3 +33,33 @@ sidebar.innerHTML = sublinks
   </article>`;
   })
   .join(``);
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener(`mouseover`, function (e) {
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    const tempPage = sublinks.find(({ page }) => page === text);
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.classList.add(`show`);
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+      submenu.innerHTML = `
+      <section>
+      <h4>${page}</h4>
+      <div class="submenu-center col-2">
+      ${links
+        .map((link) => {
+          return `<a href="${link.url}">
+      <i class="${link.icon}"></i> ${link.label}</a>`;
+        })
+        .join(``)}
+      </div>
+      </section>
+      `;
+    }
+  });
+});
